e1e45aef4b86407dbbdeaac76cae52f32a9dcdcd
Denis Istomin <istomin.den@gmail.com>
2020-03-22 03:29:13 +0500

    Add session sample

--- web/src/main/java/com/example/springwebapp/controller/Home.java
+++ web/src/main/java/com/example/springwebapp/controller/Home.java
@@ -2,6 +2,9 @@ package com.example.springwebapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Denis on 2/20/2016.
@@ -13,5 +16,13 @@ public class Home {
     @RequestMapping("/")
     public String home () {
         return "index";
+    }
+
+    @RequestMapping("/home-with-session")
+    public ModelAndView homeWithSession (HttpSession session) {
+        ModelAndView mav = new ModelAndView("index");
+        String sid = session.getId();
+        mav.addObject("sid", sid);
+        return mav;
     }
 }
